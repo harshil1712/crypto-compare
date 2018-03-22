a78a212eeb52d1376a95906f901f181b248b759d
Harshil Agrawal <ghagrawal17@gmail.com>
2018-03-22 22:51:35 +0530

    Home page basic

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <HelloWorld />
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <v-app id="inspire" dark>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid grid-list-md>
+        <v-layout row wrap>
+          <v-flex xs12 md6 v-for="data in results" :key="data.Id">
+            <v-card
+            height="250px"
+            width="250px"
+            light
+            flat>
+              <v-card-media
+                :src=baseURL+data.ImageUrl
+                height="100px"
+              >
+              </v-card-media>
+              <v-card-title primary-title>
+                <div>
+                  <div class="headline">{{data.Name}}</div>
+                </div>
+              </v-card-title>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+import axios from 'axios';
+
+  export default {
+    data: () => ({
+      drawer: false,
+      posts:[],
+      results:[],
+      baseURL: ''
+    }),
+    methods:{
+      getData(){
+        axios.get('https://min-api.cryptocompare.com/data/all/coinlist')
+        .then(response => {
+            this.posts = response['data'];
+            this.baseURL = this.posts['BaseImageUrl'];
+            console.log(this.baseURL);
+            this.posts = this.posts['Data'];
+            this.posts = Object.values(this.posts);
+            for(var i=0;i<this.posts.length;i++){
+              if(this.posts[i]['Symbol']=='ETH'||this.posts[i]['Symbol']=='BTC' ||this.posts[i]['Symbol']=='XRP'||this.posts[i]['Symbol']=='LTC')
+                this.results.push(this.posts[i]);
+                console.log(this.results);
+            }
+        })
+       }
+    },
+    created(){
+      this.getData();
     }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
